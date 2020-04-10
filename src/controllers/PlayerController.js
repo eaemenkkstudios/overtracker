484a7436ca0462a839002c7df50e4d51d08a3063
cdias900 <pedro@cdias.dev>
2020-04-09 21:52:19 -0300

    Adiciona eslint

--- src/controllers/PlayerController.js
+++ src/controllers/PlayerController.js
@@ -2,7 +2,7 @@ const ow = require('oversmash').default();
 const firebase = require('firebase-admin');
 const firebaseConfig = require('../config.json');
 
-var serviceAccount = require("../serviceAccountKey.json");
+const serviceAccount = require('../serviceAccountKey.json');
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
@@ -10,69 +10,68 @@ firebase.initializeApp({
 });
 
 async function registerBattleTag(battleTag, platform) {
-    const player = await ow.player(battleTag);
-    if(!player.accounts.length) return undefined;
-    let platformIndex = -1;
-    if(platform){
-        for(let i = 0; i < player.accounts.length; i++){
-            if(player.accounts[i].platform == platform){
-                platformIndex = i;
-            }
-        }
-    } else {
-        platformIndex = 0;
-        return (await firebase.database().ref('battletags').push({ tag: battleTag }));
+  const player = await ow.player(battleTag);
+  if (!player.accounts.length) return undefined;
+  let platformIndex = -1;
+  if (platform) {
+    for (let i = 0; i < player.accounts.length; i += 1) {
+      if (player.accounts[i].platform === platform) {
+        platformIndex = i;
+      }
     }
-    if(platformIndex < 0) return undefined;
-};
+  } else {
+    platformIndex = 0;
+    return firebase.database().ref('battletags').push({ tag: battleTag });
+  }
+  if (platformIndex < 0) return undefined;
+}
 
 module.exports = {
-    friendlyPlatforms: {
-        pc: 'PC',
-        psn: 'PlayStation Network',
-        xbl: 'Xbox Live'
-    },
-    
-    async followPlayer(req, res){
-        const token = req.headers.authorization;
-        const battleTag = req.params.battleTag.replace(/-/g, '#');
-        const { platform } = req.query;
-        firebase.auth().verifyIdToken(token)
-            .then(async userData => {
-                firebase.database().ref('battletags')
-                    .orderByChild('tag')
-                    .equalTo(battleTag)
-                    .once('value', async snapshot => {
-                        if(snapshot.val()){
-                            const battleTagId = Object.keys(snapshot.val())[0];
-                            await firebase.database().ref('accounts')
-                                .child(userData.uid)
-                                .child('following')
-                                .orderByValue()
-                                .equalTo(battleTagId)
-                                .once('value', async snap => {
-                                    if(snap.val()) return res.status(400).send();
-                                    await firebase.database()
-                                    .ref('accounts')
-                                    .child(userData.uid)
-                                    .child('following')
-                                    .push(battleTagId);
-                                    return res.status(200).send();
-                                });
-                        } else {
-                            const newBattleTag = await registerBattleTag(battleTag, platform);
-                            if(!newBattleTag) return res.status(400).send();
-                            await firebase
-                                .database()
-                                .ref('accounts')
-                                .child(userData.uid)
-                                .child('following')
-                                .push(newBattleTag.key);
-                            return res.status(201).send();
-                        }
-                        
-                    });
-            })
-            .catch(() => res.status(400).send());
-    },
-}
+  friendlyPlatforms: {
+    pc: 'PC',
+    psn: 'PlayStation Network',
+    xbl: 'Xbox Live',
+  },
+
+  async followPlayer(req, res) {
+    const token = req.headers.authorization;
+    const battleTag = req.params.battleTag.replace(/-/g, '#');
+    const { platform } = req.query;
+    firebase.auth().verifyIdToken(token)
+      .then(async (userData) => {
+        firebase.database().ref('battletags')
+          .orderByChild('tag')
+          .equalTo(battleTag)
+          .once('value', async (snapshot) => {
+            if (snapshot.val()) {
+              const battleTagId = Object.keys(snapshot.val())[0];
+              await firebase.database().ref('accounts')
+                .child(userData.uid)
+                .child('following')
+                .orderByValue()
+                .equalTo(battleTagId)
+                .once('value', async (snap) => {
+                  if (snap.val()) return res.status(400).send();
+                  await firebase.database()
+                    .ref('accounts')
+                    .child(userData.uid)
+                    .child('following')
+                    .push(battleTagId);
+                  return res.status(200).send();
+                });
+            } else {
+              const newBattleTag = await registerBattleTag(battleTag, platform);
+              if (!newBattleTag) return res.status(400).send();
+              await firebase
+                .database()
+                .ref('accounts')
+                .child(userData.uid)
+                .child('following')
+                .push(newBattleTag.key);
+              return res.status(201).send();
+            }
+          });
+      })
+      .catch(() => res.status(400).send());
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -6,15 +6,15 @@ const routes = express.Router();
 const PlayerController = require('./controllers/PlayerController');
 
 routes.post('/add/:battleTag', celebrate({
-    [Segments.HEADERS]: Joi.object({
-        authorization: Joi.string().required(),
-    }).unknown(),
-    [Segments.PARAMS]: Joi.object().keys({
-        battleTag: Joi.string().pattern(/^\D\w{2,12}-\d{4,5}$/u).required(),
-    }),
-    [Segments.QUERY]: Joi.object().keys({
-        platform: Joi.string().valid(...Object.keys(PlayerController.friendlyPlatforms)).optional(),
-    }),
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+  [Segments.PARAMS]: Joi.object().keys({
+    battleTag: Joi.string().pattern(/^\D\w{2,12}-\d{4,5}$/u).required(),
+  }),
+  [Segments.QUERY]: Joi.object().keys({
+    platform: Joi.string().valid(...Object.keys(PlayerController.friendlyPlatforms)).optional(),
+  }),
 }), PlayerController.followPlayer);
 
 module.exports = routes;
